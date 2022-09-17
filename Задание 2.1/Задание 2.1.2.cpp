901a1df1d3ba12cec3b1f9b53706167ba5fc906f
kulikovmat@yandex.ru <kulikovmat@yandex.ru>
2022-09-17 20:53:04 +0300

    Задание 2.1

---
+++ Задание 2.1/Задание 2.1.2.cpp
@@ -0,0 +1,50 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+
+	setlocale(LC_ALL, "Russian");
+
+	//Ввод знвчения x и y
+	float x;
+	cout << "Введите первое число: ";
+	cin >> x;
+	cout << "Введите второе число:";
+	float y;
+	cin >> y;
+
+	// среднее арифметическое двух введённых чисел
+	cout << "Среднее арифметическое двух введённых чисел: " << ((x + y) / 2) << endl;
+
+	//ввод знака
+	char znak;
+	cout << "Введите знак операции: +, -, * или / ";
+	cin >> znak;
+
+	//операции сложения, вычитания, умножение, деления над введенными значениями 
+	switch (znak)
+	{
+	case '+':
+		cout << "x + y = " << x + y << endl;
+		break;
+	case '-':
+		cout << "x - y = " << x - y << endl;
+		break;
+	case '*':
+		cout << "x * y = " << x * y << endl;
+		break;
+	case '/':
+		cout << "x / y = " << x / y << endl;
+		break;
+	default:
+		cout << "Ошибка" << endl;
+	}
+
+
+}
