7000d32713abcb1f312e234320dc910114a1af8a
kulikovmt <113171889+kulikovmt@users.noreply.github.com>
2022-12-19 22:16:22 +0300

    Delete Characteristic.cpp

--- Задание 4/Characteristic.cpp
+++
@@ -1,9 +0,0 @@
-#include "Characteristic.hpp"
-#include "Gun.hpp"
-
-Characteristic::Characteristic(float power) : power(power) {}
-
-
-float Characteristic::getDamage(Gun& gun) {
-	return this->power + gun.damage;
-}
